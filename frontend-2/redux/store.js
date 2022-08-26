9818cc7a4526502feb8af52a1c1c78db506747ac
levietdat <36151272+levietdat@users.noreply.github.com>
2022-08-26 12:06:02 +0700

    update

--- frontend-2/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import userReducer from './userSlice'
-const store = configureStore({
-   reducer:{
-    userReducer:userReducer
-   } 
-})
-
-export default store
